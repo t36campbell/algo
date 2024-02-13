0ee0a54844a32b02c2b27741743adaee1aa43671
t36campbell <campbell3636@yahoo.com>
2024-02-12 23:12:27 -0500

    working on array

--- src/ds/array.rs
+++ src/ds/array.rs
@@ -9,13 +9,25 @@ pub struct DynamicArray<'a, T> {
     len: usize,
 }
 
-impl<'a,T> Array<T> for DynamicArray<'a,T> {
+impl<'a, T> Default for DynamicArray<'a, T>
+where
+    T: Copy + Default,
+{
+    fn default() -> DynamicArray<'a, T> {
+        todo!()
+    }
+}
+
+impl<'a, T> Array<T> for DynamicArray<'a, T> {
     // where the last item in memory is - not the actual length of the array
     fn len(&self) -> usize {
         todo!()
     }
 
     fn get(&self, index: usize) -> Option<&T> {
+        if index > self.len {
+            todo!("raise index out of bounds")
+        }
         match self.array.get(index) {
             Some(v) => v.as_ref(),
             None => None,
@@ -27,26 +39,30 @@ impl<'a,T> Array<T> for DynamicArray<'a,T> {
     }
 }
 
-impl<'a,T> DynamicArray<'a,T> where T: Copy + Default {
+impl<'a, T> DynamicArray<'a, T>
+where
+    T: Copy + Default,
+{
     fn init(array: Option<&'a mut [Option<T>]>) -> Self {
         let array: &mut [Option<T>] = match array {
             Some(a) => a,
-            None => [None; 64].as_mut(),
+            None => Self::default().array,
         };
-
-        DynamicArray { array, len: 0 }
+        Self { array, len: 0 }
     }
 
     fn new() -> Self {
         Self::init(None)
     }
-}
 
+    fn size(&self) -> usize {
+        return self.array.len();
+    }
+}
 
 #[cfg(test)]
 mod tests {
 
     #[test]
-    fn dynamic_array() {
-    }
+    fn dynamic_array() {}
 }
