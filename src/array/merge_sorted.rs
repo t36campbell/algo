c7dd518b1457107ca44c7b823635b431a7678bae
t36campbell <campbell3636@yahoo.com>
2024-02-18 22:39:08 -0500

    solved #88

--- src/array/merge_sorted.rs
+++ src/array/merge_sorted.rs
@@ -1,55 +1,35 @@
+use crate::sort;
+
+/// Solution for LeetCode 88. Merge Sorted Array
 pub fn merge<'a>(nums1: &'a mut Vec<i32>, m: i32, nums2: &'a mut Vec<i32>, n: i32) {
-    // assumptions:
-    // m and n are always accurate
-    nums1.retain(|&i|i != 0);
+    if m < nums1.len() as i32 {
+        let binding = nums1.clone();
+        let (n1, _) = binding.split_at(m as usize);
+        nums1.clear();
+        nums1.append(&mut n1.to_vec());
+    }
+
+    if n < nums2.len() as i32 {
+        let binding = nums2.clone();
+        let (n2, _) = binding.split_at(n as usize);
+        nums2.clear();
+        nums2.append(&mut n2.to_vec());
+    }
+
     if n <= 0 {
         return;
     }
-    
+
     if m <= 0 {
         nums1.clear();
         nums1.append(nums2);
-        nums1.retain(|&i|i != 0);
         return;
     }
 
-    let zero = 0_i32;
-    let mut index = 0;
-    let mut inserts = 0;
-    let max = if m > n { m } else { n } as usize;
-    while index <= max {
-        let ga = nums1.last().unwrap();
-        let offset = index + inserts;
-        let a = nums1.get(offset).unwrap_or(&zero);
-        let b = nums2.get(index).unwrap_or(&zero);
-
-        if a == &zero && b == &zero {
-            let el = if a == &zero { *a } else { *b };
-            nums1.insert(0,  el);
-            inserts += 1;
-            index += 1;
-            continue;
-        }
-
-        if b > ga {
-            nums1.push(*b);
-            index += 1;
-            continue;
-        };
-
-        let length = nums1.len();
-        let insert = if b > a { offset + 1 } else { offset };
-        if insert > length {
-            nums1.push(*b);
-            continue;
-        };
-    
-        nums1.insert(insert,  *b);
-        inserts += 1;
-        index += 1;
-    }
-
-    nums1.retain(|&i|i != 0);
+    nums1.append(nums2);
+    let mut merged = sort::merge(nums1.to_vec());
+    nums1.clear();
+    nums1.append(&mut merged);
 }
 
 #[cfg(test)]
@@ -61,86 +41,88 @@ mod tests {
         let mut one = vec![1];
         let mut two = vec![];
         merge(&mut one, 1, &mut two, 0);
-        
-        print!("\n{:?}\n", one);
+
         assert_eq!(one, vec![1]);
         assert_eq!(one.len(), 1);
     }
-    
+
     #[test]
     fn e2() {
         let mut one = vec![0];
         let mut two = vec![1];
         merge(&mut one, 0, &mut two, 1);
-        
-        print!("\n{:?}\n", one);
+
         assert_eq!(one, vec![1]);
         assert_eq!(one.len(), 1);
     }
-    
+
     #[test]
     fn e3() {
-        let mut one = vec![2,0];
+        let mut one = vec![2, 0];
         let mut two = vec![1];
         merge(&mut one, 1, &mut two, 1);
-        
-        print!("\n{:?}\n", one);
-        assert_eq!(one, vec![1,2]);
+
+        assert_eq!(one, vec![1, 2]);
         assert_eq!(one.len(), 2);
     }
-    
+
     #[test]
     fn e4() {
         let mut one = vec![1, 2, 3, 4];
         let mut two = vec![5, 6, 7, 8];
         merge(&mut one, 4, &mut two, 4);
-        
-        print!("\n{:?}\n", one);
+
         assert_eq!(one.len(), 8);
         assert_eq!(one, vec![1, 2, 3, 4, 5, 6, 7, 8]);
     }
-    
+
     #[test]
     fn e5() {
         let mut one = vec![1, 2, 3];
         let mut two = vec![2, 5, 6];
         merge(&mut one, 3, &mut two, 3);
 
-        print!("\n{:?}\n", one);
         assert_eq!(one.len(), 6);
         assert_eq!(one, vec![1, 2, 2, 3, 5, 6]);
     }
-    
+
     #[test]
     fn e6() {
         let mut one = vec![1, 3, 5, 7];
         let mut two = vec![2, 4, 6, 8];
         merge(&mut one, 3, &mut two, 3);
-        
-        print!("\n{:?}\n", one);
+
         assert_eq!(one.len(), 8);
         assert_eq!(one, vec![1, 2, 3, 4, 5, 6, 7, 8]);
     }
-    
+
     #[test]
     fn e7() {
         let mut one = vec![1, 2, 3, 0, 0, 0];
         let mut two = vec![2, 5, 6];
         merge(&mut one, 3, &mut two, 3);
 
-        print!("\n{:?}\n", one);
         assert_eq!(one.len(), 6);
         assert_eq!(one, vec![1, 2, 2, 3, 5, 6]);
     }
 
     #[test]
     fn e8() {
-        let mut one = vec![4,0,0,0,0,0];
-        let mut two = vec![1,2,3,5,6];
+        let mut one = vec![4, 0, 0, 0, 0, 0];
+        let mut two = vec![1, 2, 3, 5, 6];
         merge(&mut one, 1, &mut two, 5);
 
-        print!("\n{:?}\n", one);
         assert_eq!(one.len(), 6);
         assert_eq!(one, vec![1, 2, 3, 4, 5, 6]);
+    }
+
+    #[test]
+    fn e9() {
+        let mut one = vec![-1, 0, 0, 3, 3, 3, 0, 0, 0];
+        let mut two = vec![1, 2, 2];
+        merge(&mut one, 6, &mut two, 3);
+
+        assert_eq!(one.len(), 9);
+        assert_eq!(one, vec![-1, 0, 0, 1, 2, 2, 3, 3, 3]);
     }
 }
