98e501db31b438969deb9ab939e47bcff1de93b8
t36campbell <campbell3636@yahoo.com>
2024-07-02 09:14:40 -0400

    updated lib to fit usage

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
+mod ds;
+mod search;
+mod sort;
+mod tree;
+
 pub mod array;
 pub mod dp;
-pub mod ds;
-pub mod search;
-pub mod sort;
-pub mod tree;
+pub mod matrix;
+
+pub use ds::*;
+pub use search::*;
+pub use sort::*;
+pub use tree::*;
+
+pub mod utils;
